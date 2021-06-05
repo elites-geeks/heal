a0793da8e5c1fb2ef780e2578fcb21d6c9f77401
abu-al3ees <malkhazali83@gmail.com>
2021-06-05 13:56:36 +0300

    add basic auth and barear

---
+++ src/middlewares/auth/bearer.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const UserModel = require('../models/user');
+
+module.exports = async (req, res, next) => {
+  // a token in the req headers 
+  // Authorization header value will be "Bearer token"
+  if (!req.headers.authorization) {
+    next('Not Logged-in user');
+    // res.status(403).send()
+  } else {
+    // get the token from headers
+    try {
+      let token = req.headers.authorization.split(' ').pop();
+      console.log('token in Bearer Auth:: ', token);
+      let user = await UserModel.authenticateToken(token);
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        next('Invalid Token!!!!');
+      }
+
+    } catch(ex) {
+      // next('Invalid User Token ')
+      res.status(403).send('Invalid Token!!!!');
+
+    }
+  }
+        
+};
