9f9db8a2a12fb0144f94ecd3680bf32f7cd7632f
areenjaradat <areenjaradat96@gmail.com>
2021-06-07 21:07:41 +0300

    working in routing

--- src/server/server.js
+++ src/server/server.js
@@ -16,49 +16,49 @@ app.use(express.static(path.join(__dirname, '../../public')));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: true,
+  extended: true,
 }));
 
 
 
 // const patient=require('../server/routes/patient.js');
 // const doctor=require('../server/routes/doctor.js');
-// const insurance=require('../server/routes/insurance');
+const insurance=require('../server/routes/insurance');
 
 // app.get('/', (req, res) => {
 //   res.sendFile('../../public/home.html');
 // });
 app.post('/signin', basic, (req, res) => {
-    // const userRole = req.user.field? req.user.role+'/'+req.user.field : req.user.role;
+  // const userRole = req.user.field? req.user.role+'/'+req.user.field : req.user.role;
 
-    console.log("Authintication done");
-    res.send(req.user)
+  console.log('Authintication done');
+  res.send(req.user);
 });
 app.post('/regesterUser', bearer, (req, res) => {
-    const {
-        user,
-        token
-    } = req;
-    const output = {
-        user,
-        token
-    }
-    res.send(output)
+  const {
+    user,
+    token,
+  } = req;
+  const output = {
+    user,
+    token,
+  };
+  res.send(output);
 });
 
 // app.use('/patient',patient);
 // app.use('/doctor',doctor);
 
-// app.use('/insurance',insurance);
+app.use('/insurance',insurance);
 
 function run(PORT) {
-    app.listen(PORT, () => {
-        console.log('Server up on ', PORT);
-    });
+  app.listen(PORT, () => {
+    console.log('Server up on ', PORT);
+  });
 }
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 module.exports = {
-    run,
-    app,
+  run,
+  app,
 };
